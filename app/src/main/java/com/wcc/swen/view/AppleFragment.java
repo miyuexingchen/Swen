0f4b3dfd313763e76f26b9f18705b0fe20777010
wangchenchen <miyuexingchen@gmail.com>
2016-12-13 17:40:43 +0800

    增加apple。

---
+++ app/src/main/java/com/wcc/swen/view/AppleFragment.java
@@ -0,0 +1,40 @@
+package com.wcc.swen.view;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.wcc.swen.R;
+import com.wcc.swen.contract.AppleContract;
+import com.wcc.swen.presenter.ApplePresenter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AppleFragment extends Fragment {
+
+    @BindView(R.id.tv_apple_fragment)
+    TextView tv_apple_fragment;
+
+    private AppleContract.Presenter mPresenter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_apple, container, false);
+        ButterKnife.bind(this, view);
+
+        mPresenter = new ApplePresenter();
+        mPresenter.dailyApple(getActivity(), tv_apple_fragment);
+
+        return view;
+    }
+
+}
